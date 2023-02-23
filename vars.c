7560f51635b84280a1a3f6745624e2991bf0ed36
nahom <derebeam2123@gmail.com>
2023-02-23 02:13:52 -0800

    task 16

--- vars.c
+++ vars.c
@@ -23,10 +23,10 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		j++;
 		info->cmd_buf_type = CMD_AND;
 	}
-	else if	 (buf[j] == ';')
+	else if (buf[j] == ';')
 	{
 		buf[j] = 0;
-		innfo->cmd_bud_type = CMD_CHAIN;
+		info->cmd_bud_type = CMD_CHAIN;
 	}
 	else
 		return (0);
@@ -104,31 +104,32 @@ int replace_vars(info_t *info)
 {
 	int i = 0;
 	list_t *node;
+
 	for (i = 0; info->argv[i]; i++)
 	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1])
 			continue;
+		
 		if (!_strcmp(info->argv[i], "$?"))
 		{
 			replace_string(&(info->argv[i]),
-				_strdup(convert_number((info->status, 10 0)));
+				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
 		if (!_strcmp(info->argv[i], "$$"))
 		{
-		        replace_string(&(info->argv[i]),
-				_strdup(convert_number(getpid(), 10, 0)))
-			continue;
+			replace_string(&(info->argv[i]),
+					_strdup(convert_number(getpid(), 10, 0)));
+		continue;
 		}
 		node = node_starts_with(info->env, &info->argv[i][1], '=');
 		if (node)
 		{
-		replace_string(&(info->argv[i]),
-				_stdup(_strchr(node->str, '=') +));
-		continue;
+			replace_string(&(info->argv[i]),
+				_strdup(_strchr(node->str, '=') + 1));
+			continue;
 		}
-		replace_string(&info->argv[i], _strdup(""));
-
+		replace_string(&info->argv[i], _strdup(""));	
 	}
 	return (0);
 }
@@ -139,7 +140,7 @@ int replace_vars(info_t *info)
 * @new: new string
 * Return: 0 or 1
 */
-int replace_string(char **old, char, char *new)
+int replace_string(char **old, char *new)
 {
 	free(*old);
 	*old = new;
