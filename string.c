b454cf0f904d5350194dc1af1318b3533c2230d8
nahom <derebeam2123@gmail.com>
2023-02-23 04:07:58 -0800

    task 19

--- string.c
+++ string.c
@@ -1,1 +1,73 @@
+#include "shell.h"
 
+/**
+ * _strlen - return length
+ * @s: string
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * _strcmp - perform
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: negative
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - start
+ * @haystack: string
+ * @needle: find
+ *
+ * Return: address
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+/**
+ * _strcat - string
+ * @dest: the destinction
+ * @src: buffer
+ *
+ * Return: ponter
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
