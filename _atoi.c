19d8646224e0dda48fce2f685c1cae0ac0e6c841
nahom <derebeam2123@gmail.com>
2023-07-11 04:02:52 -0700

    task 1

--- _atoi.c
+++ _atoi.c
@@ -2,7 +2,7 @@
 
 /**
  * interactive - returns true if shell is interactive mode
- * @info:struct address
+ * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
@@ -12,7 +12,7 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks is if character is delimeter
+ * is_delim - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
@@ -26,10 +26,11 @@ int is_delim(char c, char *delim)
 }
 
 /**
- * _isalpha - check for alphabetic character
- * @c: the character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -37,17 +38,19 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, convert number
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -58,9 +61,10 @@ int _atoi(char *s)
 			result *= 10;
 			result += (s[i] - '0');
 		}
-	else if (flag == 1)
-		flag = 2;
+		else if (flag == 1)
+			flag = 2;
 	}
+
 	if (sign == -1)
 		output = -result;
 	else
